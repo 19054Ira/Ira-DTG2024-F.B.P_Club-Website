46f66bd7a6f1df25aaf4ec5f044ecb02e616b65f
19054Ira <163358920+19054Ira@users.noreply.github.com>
2024-07-04 11:57:55 +1200

    Registration page html and python code

--- website/models.py
+++ website/models.py
@@ -15,3 +15,14 @@ class Note(db.Model):
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    position = db.Column(db.String(50))
+    
+
+    
+
+    
